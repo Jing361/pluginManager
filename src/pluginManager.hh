d035f73a14f876836cd3c2cd6428b9379ab4553f
Chris Samuelson <chris.sam55@gmail.com>
2015-09-12 11:09:23 -0400

    Moved typedefs from pmDefs to pluginManager header.

--- src/pluginManager.hh
+++ src/pluginManager.hh
@@ -1,12 +1,12 @@
-#ifndef __PLUGIN_MANAGER_H__
-#define __PLUGIN_MANAGER_H__
+#ifndef __PLUGIN_MANAGER_HH__
+#define __PLUGIN_MANAGER_HH__
 
 #include<map>
 #include<vector>
-#include<iostream>
 #include<string>
+#include<iostream>
+#include<functional>
 #include<dlfcn.h>
-#include"pmDefs.hh"
 
 template<class T>
 class pluginManager{
@@ -17,8 +17,34 @@ private:
   std::vector<void*> handles;
 
 public:
+  typedef T type;
+  typedef unsigned char byte_t;
+  typedef std::function<T*()> create_t;
+  typedef std::function<void(T*)> delete_t;
+  typedef std::function<char*()> name_t;
+  //Provided by plugin to register object.
+  typedef struct{
+    version_t version;
+    create_t create;
+    delete_t destroy;
+  }registerParams;
+  //Manager calls the initFunc of a plugin,
+  //  from which the plugin registers objects.
+  typedef void (*initFunc_t)(const platformServices*);
+  typedef struct{
+    version_t version;
+    registerFunc registerObject;
+  }platformServices;
+  typedef struct{
+    unsigned int major;
+    unsigned int minor;
+    unsigned int maint;
+  }version_t;
+  typedef std::function<int(const byte_t*, const registerParams*)> registerFunc;
+
   pluginManager();
   virtual ~pluginManager();
+
   int load(const char* dir);
   int load(std::string dir);
   int loadall(const char* dir);

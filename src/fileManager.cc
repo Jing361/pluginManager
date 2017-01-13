4aa7ad5d90bc2062f41ac5cc7bf659e0a0d32852
Chris Samuelson <chris.sam55@gmail.com>
2017-01-12 21:59:09 -0500

    Style updates

--- src/fileManager.cc
+++ src/fileManager.cc
@@ -3,26 +3,26 @@
 #include<dirent.h>
 #include"fileManager.hh"
 
-std::vector<std::string> fileManager::getFiles(const std::string& dir){
+std::vector<std::string> fileManager::getFiles( const std::string& dir ){
   DIR *dp;
   struct dirent *dirp;
-  std::string bad1(".");
-  std::string bad2("..");
+  std::string bad1( "." );
+  std::string bad2( ".." );
   std::vector<std::string> files;
 
-  if((dp  = opendir(dir.c_str())) == NULL) {
-    std::cerr << "Error(" << errno << ") opening " << dir << std::endl;
+  if( ( dp  = opendir( dir.c_str() ) ) == NULL ) {
+    std::cerr << "Error( " << errno << " ) opening " << dir << std::endl;
     return files;
   }
 
-  while ((dirp = readdir(dp)) != NULL) {
-    std::string tmp(dirp->d_name);
-    if(tmp != bad1 && tmp!= bad2){
-      files.push_back(tmp);
+  while ( ( dirp = readdir( dp ) ) != NULL ) {
+    std::string tmp( dirp->d_name );
+    if( tmp != bad1 && tmp!= bad2 ){
+      files.push_back( tmp );
     }
   }
 
-  closedir(dp);
+  closedir( dp );
   return files;
 }
 
--- src/fileManager.hh
+++ src/fileManager.hh
@@ -1,9 +1,12 @@
 #ifndef __FILE_MANAGER_H__
 #define __FILE_MANAGER_H__
 
+#include<string>
+
+//TODO: update to use c++ 17 file system API
 class fileManager{
 public:
-  static std::vector<std::string> getFiles(const std::string& dir);
+  static std::vector<std::string> getFiles( const std::string& dir );
 };
 
 #endif
